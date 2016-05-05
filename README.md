59b8c28b0d8032f0ae703c2649dacb9fefebe37c
Marc Juul <github@juul.io>
2016-05-05 01:17:51 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+WORK IN PROGRESS. NOTHING WORKS YET!
+
+These are some scripts and config files to make it easier to compile the fread initrd (initial ramdisk).
+
+# Compiling
+
+Documentation is available in the readme file for [fread-vagrant](https://github.com/fread-ink/fread-vagrant) and I highly suggest you use that vagrant system rather than trying to install the dependencies directly on your system.
+
+# Copyright and license
+
+Unless otherwise stated everything in this repository Copyright 2016 Marc Juul and licensed under the [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+# Disclaimer
+
+Kindle and Lab126 are registered trademarks of Amazon Inc. 
+
+Kobo is a registered trademark of Kobo Inc. 
+
+Nook is a registered trademark of Barnes & Noble Booksellers Inc. 
+
+E Ink is a registered trademark of the E Ink Corporation. 
+
+None of these organizations are in any way affiliated with fread nor this git project nor any of the authors of this project and neither fread nor this git project is in any way endorsed by these corporations.
---
+++ fetch.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+BUILDROOT_BASEURL="https://buildroot.org/downloads/"
+BUILDROOT_NAME="buildroot-2016.02"
+BUILDROOT_FILENAME="${BUILDROOT_NAME}.tar.bz2"
+BUILDROOT_URL="${BUILDROOT_BASEURL}${BUILDROOT_FILENAME}"
+BUILDROOT_CONFIG="./buildroot.config"
+BUSYBOX_CONFIG="./busybox.config"
+
+# check if we're in a subdir of /vagrant
+if [[ `pwd` = /vagrant/* ]]; then
+  echo "You cannot compile the fread initrd from inside /vagrant" >&2
+  echo "since that directory is shared between vm and host" >&2
+  echo "in a way that does not allow hardlinks and the" >&2
+  echo "buildroot build process requires the use of hardlink." >&2
+  exit 1
+fi
+
+if [ ! -f "$BUILDROOT_CONFIG" ]; then
+  echo "The buildroot config file ${BUILDROOT_CONFIG} is missing!" >&2
+  exit 1
+fi
+
+if [ ! -f "$BUSYBOX_CONFIG" ]; then
+  echo "The busybox config file ${BUSYBOX_CONFIG} is missing!" >&2
+  exit 1
+fi
+
+if [ -f "$BUILDROOT_FILENAME" ]; then
+  echo "Buildroot has already been downloaded."
+else
+
+  echo "Downloading buildroot..."
+
+  wget $BUILDROOT_URL
+
+  if [ "$?" -ne 0 ]; then
+    echo "Downloading buildroot failed" >&2
+    exit 1
+  fi
+fi
+
+echo "Downloading complete!"
+
+if [ -d "$BUILDROOT_NAME" ]; then
+  echo "The directory ${BUILDROOT_NAME} already exists." >&2
+  echo "If you wish to restart the build process from scratch" >&2
+  echo "please move to a different directory or delete the entire" >&2
+  echo "${BUILDROOT_NAME} directory but know that this will cause" >&2
+  echo "all of the buildroot toolchain and packages" > &2
+  echo "to be re-downloaded and re-built" > &2
+  exit 1
+fi
+
+echo "Extracting buildroot..."
+
+tar xjf $BUILDROOT_FILE
+
+if [ "$?" -ne 0 ]; then
+  echo "Extracting buildroot failed" >&2
+  exit 1
+fi
+
+echo "Extraction complete!"
+
+echo "Configuring buildroot..."
+
+cp $BUILDOOR_FILE ${BUILDROOT_NAME}/.config
+
+if [ "$?" -ne 0 ]; then
+  echo "Configuring buildroot failed" >&2
+  exit 1
+fi
+
+echo "Buildroot configured!"
+echo "to build simply run ./build.sh"
+
